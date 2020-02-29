ca3b3c66968b40fad24aab3cb82e5983a2a5121f
Daniil <36066254+DaniilOr@users.noreply.github.com>
2020-02-29 14:51:18 +0600

    Post query templates

--- EcoHack/app/routes.py
+++ EcoHack/app/routes.py
@@ -1,6 +1,33 @@
 from app import app
+from flask import render_template
+from flask import request
+
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+
+@app.route('/add_box', methods=['GET', 'POST'])
+def add_box():
+    box_id = request.args.get('box_id')
+    return box_id
+
+@app.route('/delete_box', methods=['GET', 'POST'])
+def delete_box():
+    box_id = request.args.get('box_id')
+    return box_id
+
+
+@app.route('/box_is_full', methods=['GET', 'POST'])
+def box_is_full():
+    box_id = request.args.get('box_id')
+    return box_id
+
+
+@app.route('/clear_box', methods=['GET', 'POST'])
+def clear_box():
+    box_id = request.args.get('box_id')
+    return box_id
