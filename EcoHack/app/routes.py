2d17e853531017c107515acfbcc516a314eb4668
e-kondr01 <e.kondr01@gmail.com>
2020-02-29 13:33:38 +0300

    routes test

--- EcoHack/app/routes.py
+++ EcoHack/app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template
 from flask import request
+from app.models import db, Building, Place, Boxe
 
 
 
@@ -12,22 +13,28 @@ def index():
 
 @app.route('/add_box', methods=['GET', 'POST'])
 def add_box():
-    box_id = request.args.get('box_id')
-    return box_id
+    new_box = Boxe(place_id)
+    db.session.add(new_box)
+    db.session.commit()
+    return "Yay"
 
 @app.route('/delete_box', methods=['GET', 'POST'])
-def delete_box():
-    box_id = request.args.get('box_id')
-    return box_id
+def delete_box(id: int) -> None:
+    """Удаляет коробку из базы данных """
+    box = Box.query.get(id)
+    db.session.delete(box)
+    db.session.commit()
 
 
 @app.route('/box_is_full', methods=['GET', 'POST'])
-def box_is_full():
-    box_id = request.args.get('box_id')
-    return box_id
+def box_is_full(id: int) -> None:
+    box = Box.query.get(id)
+    box.is_full = True
+    db.session.commit()
 
 
 @app.route('/clear_box', methods=['GET', 'POST'])
-def clear_box():
-    box_id = request.args.get('box_id')
-    return box_id
+def clear_box(id: int) -> None:
+    box = Box.query.get(id)
+    box.is_full = False
+    db.session.commit()
