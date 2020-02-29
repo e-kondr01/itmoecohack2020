e66dd2e3982973f61ed23280acb0477be54ed742
Daniil <36066254+DaniilOr@users.noreply.github.com>
2020-02-29 16:17:04 +0600

    Add files via upload

--- EcoHack/app/routes.py
+++ EcoHack/app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template
 from flask import request
+from app.models import db, Building, Place, Boxe
 
 
 
@@ -12,8 +13,10 @@ def index():
 
 @app.route('/add_box', methods=['GET', 'POST'])
 def add_box():
-    box_id = request.args.get('box_id')
-    return box_id
+    new_box = Boxe(place_id)
+    db.session.add(new_box)
+    db.session.commit()
+    return "Yay"
 
 @app.route('/delete_box', methods=['GET', 'POST'])
 def delete_box():
